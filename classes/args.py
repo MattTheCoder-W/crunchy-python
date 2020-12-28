dc16afeb61ebcb0ac2366384a5932a2f86b133c7
Matthew The Coder <76630571+MattTheCoder-W@users.noreply.github.com>
2020-12-28 21:11:59 +0100

    Added modules

--- classes/args.py
+++ classes/args.py
@@ -1,6 +1,43 @@
 import argparse
+from classes.interactive import Interactive
+from classes.message import message
 
 
 class Arguments:
     def __init__(self):
-        pass  # For later
+        ap = argparse.ArgumentParser()
+        ap.add_argument('-m', '--min', type=str, help="Minimum length of wordlist word")
+        ap.add_argument('-x', '--max', type=str, help="Maximum length of wordlist word")
+        ap.add_argument('-c', '--chars', type=str, help="Characters for generating words")
+        ap.add_argument('-o', '--output', type=str, help="Output file (if needed)")
+        ap.add_argument('-i', '--interactive', action="store_true", help="Interactive mode of this script")
+        self.args = vars(ap.parse_args())
+        self.interactive = self.args['interactive']
+        if self.interactive:
+            # Open interactive mode
+            self.args = Interactive().getargs()
+        self.checkargs()
+
+    def getargs(self):
+        return self.args
+
+    def checkargs(self):
+        doexit = False
+        if self.args['min'] in [None, ""]:
+            message("Please specify minimum length!", "error")
+            doexit = True
+        if self.args['max'] in [None, ""]:
+            message("Please specify maximum length!", "error")
+            doexit = True
+        if self.args['chars'] in [None, ""]:
+            message("Please specify characters!", "error")
+            doexit = True
+        if doexit:
+            exit()
+        if int(self.args['min']) > int(self.args['max']):
+            message("Min length cannot be bigger than max length, you idiot...", "error")
+            exit()
+        if self.args['output'] in [None, ""]:
+            self.args['notsaving'] = True
+        else:
+            self.args['notsaving'] = False
---
+++ classes/interactive.py
@@ -0,0 +1,68 @@
+from os import get_terminal_size
+from random import choice
+from classes.message import message
+
+QUOTES = ['Why are you beating me so hard?', "i don't have infinite memory...",
+                  "Please, nothing longer that 500*10^6",
+                  "Hi, friend!", "Hello?", "Matt had created me!", "I see you (눈_눈)"]
+WIDTH = get_terminal_size()[0]
+SMALL = "abcdefghijklmnopqrstuvwxyz"
+BIG = SMALL.upper()
+DIGITS = "0123456789"
+
+
+class Interactive:
+    def __init__(self):
+        self.showbanner(quiet=True)
+        self.args = self.getdata()
+
+    def getargs(self):
+        return self.args
+
+    @staticmethod
+    def getdata():
+        try:
+            message("Minimum word length: ", "question", end="")
+            in_min = str(input())
+            message("Maximum word length: ", "question", end="")
+            in_max = str(input())
+            while True:
+                message("Characters in words ('<h>' for help): ", "question", end="")
+                in_chars = str(input())
+                if in_chars == "<h>":
+                    message("Custom commands for characters choice!", "info", prefix="\t")
+                    message("<A> for big alphabet", "info", prefix="\t")
+                    message("<a> for small alphabet", "info", prefix="\t")
+                    message("<0> for digits", "info", prefix="\t")
+                    message("You can mix them! ex: <a><0><A> --> 'abcd....0123...ABCD...'", "info", prefix="\t")
+                    continue
+                else:
+                    in_chars = in_chars.replace('<a>', SMALL).replace("<A>", BIG).replace("<0>", DIGITS)
+                    break
+            message("File to save wordlist in(Empty for not saving): ", "question", end="")
+            filenm = str(input())
+            if filenm == "":
+                notsaving = True
+                message("Not saving mode ON!", "info")
+            else:
+                notsaving = False
+            return {"min": in_min, "max": in_max, "chars": in_chars, "output": filenm, 'notsaving': notsaving}
+        except KeyboardInterrupt:
+            message("Stopping", "warning", prefix="\n")
+            exit()
+
+    @staticmethod
+    def showbanner(quiet=False):
+        banner_width = 97
+        center = int((WIDTH - banner_width) / 2)
+        if not quiet:
+            with open('banner.txt', 'rb') as infile:
+                for line in infile.readlines():
+                    print(" " * center + line.decode().replace("\n", ''))
+        for j in range(3):
+            if j % 2 == 0:
+                print(" " * center + "=" * banner_width)
+            else:
+                quote = choice(QUOTES)
+                center_quote = int((banner_width - len(quote)) / 2) - 2
+                print(" " * center + ">" * center_quote + " " + quote + " " + ">" * (center_quote + 2))
